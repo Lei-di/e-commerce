2fc55ddcd14b0bd8fc64fc35f99313985d67339a
ijosepedro <josepedrod325@gmail.com>
2025-09-11 14:08:41 -0400

    Mundanca na estrutura de pastas para public

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Inclui o Router
-require_once 'configuracao/Router.php';
+require_once __DIR__ . '/../configuracao/Router.php';
 
 // Pega o path da URL acessada
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
