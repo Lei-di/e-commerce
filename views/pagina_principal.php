13b0aa15d0e206156f15c1ba979eaf7533bf236f
Leidi <leidineiacruz1@gmail.com>
2025-09-17 22:37:52 -0300

    restaurado

--- views/pagina_principal.php
+++ views/pagina_principal.php
@@ -19,10 +19,54 @@
         </div>
     </section>
 
+    <section class="menu">
+        <a class="selecao" href="">Todos</a>
+        <a href="">Vestidos</a>
+        <a href="">Blusas</a>
+        <a href="">Calças</a>
+        <a href="">Blazers</a>
+    </section>
+
     <main>
         <div class="filtro">
             <h3>Filtros</h3>
+
+            <h4 class="categoria">Categoria</h4>
+            <div class="opcoes">
+                <p><input type="checkbox" id="vestidos"><label for="vestidos">Vestidos</label></p>
+                <p><input type="checkbox" id="blusas"><label for="blusas">Blusas</label></p>
+                <p><input type="checkbox" id="calcas"><label for="calcas">Calças</label></p>
+                <p><input type="checkbox" id="blazers"><label for="blazers">Blazers</label></p>
+                <p><input type="checkbox" id="casacos"><label for="casacos">Casacos</label></p>
             </div>
+
+            <h4>Faixa de preço</h4><br>
+            <article class="preco">
+                <label><input type="number" name="minimo" value="" placeholder="Min.."></label>
+                <label><input type="number" name="maximo" value="" placeholder="Max.."></label>
+            </article>
+
+            <h4>Tamanhos</h4><br>
+            <article class="tamanho">
+                <label><input type="checkbox" name="tamanho" value="PP"> PP</label>
+                <label><input type="checkbox" name="tamanho" value="P"> P</label>
+                <label><input type="checkbox" name="tamanho" value="M"> M</label>
+                <label><input type="checkbox" name="tamanho" value="G"> G</label>
+                <label><input type="checkbox" name="tamanho" value="GG"> GG</label>
+                <label><input type="checkbox" name="tamanho" value="XG"> XG</label>
+            </article>
+
+            <h4>Cores</h4><br>
+            <article class="tamanho">
+                <label><input type="checkbox" name="cor" value="Preto"> Preto</label>
+                <label><input type="checkbox" name="cor" value="Cinza"> Cinza</label>
+                <label><input type="checkbox" name="cor" value="Vermelho"> Vermelho</label>
+                <label><input type="checkbox" name="cor" value="Branco"> Branco</label>
+                <label><input type="checkbox" name="cor" value="Azul"> Azul</label>
+                <label><input type="checkbox" name="cor" value="Verde"> Verde</label>
+            </article>
+        </div>
+
        <div class="area-produto">
             <h1>Todos</h1>
             <div class="lista-produtos">
