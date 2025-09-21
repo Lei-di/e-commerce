91c8eafa05eead07eda45fe39275948d5674b1f5
Leidineia da Cruz <101343514+Lei-di@users.noreply.github.com>
2025-09-21 17:13:38 -0300

    Update cabecalho.php

--- views/components/cabecalho.php
+++ views/components/cabecalho.php
@@ -1,12 +1,19 @@
+<style>
+    /* Adicionado para que os links não fiquem com a decoração padrão (sublinhado) */
+    .cabecalho ul li a {
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
 <section class="cabecalho">
     <div class="logo">
         <a href="<?= BASE_URL ?>/">ELEGANCIA</a>
     </div>
     <ul>
-        <li><h4>Novidades</h4></li>
-        <li><h4>Feminino</h4></li>
-        <li><h4>Masculino</h4></li>
-        <li><h4>Acessórios</h4></li>
+        <li><a href="<?= BASE_URL ?>/?categoria=novidades"><h4>Novidades</h4></a></li>
+        <li><a href="<?= BASE_URL ?>/?categoria=feminino"><h4>Feminino</h4></a></li>
+        <li><a href="<?= BASE_URL ?>/?categoria=masculino"><h4>Masculino</h4></a></li>
+        <li><a href="<?= BASE_URL ?>/?categoria=acessorios"><h4>Acessórios</h4></a></li>
     </ul>
     <div class="pesquisa">
         <img src="<?= BASE_URL ?>/assets/icones/lupa.png" alt="Ícone de busca">
