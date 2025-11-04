71d44542be4bb4dcd5368311628407fe9787c366
FranBordinhao <franbordinhaomendonca@gmail.com>
2025-11-03 23:45:30 -0400

    Melhorar sistema de filtros por categoria - adicionar debug e verificação

--- controllers/web/HomeController.php
+++ controllers/web/HomeController.php
@@ -8,9 +8,15 @@ class HomeController {
         $produtos = [];
         $titulo = "Todos";
 
+        // DEBUG: Mostrar qual categoria foi recebida
+        // echo "<pre>Categoria recebida: " . var_export($categoria, true) . "</pre>";
+
         if ($categoria) {
             $produtos = Produto::getByCategoria($categoria);
             $titulo = ucfirst($categoria); // Deixa a primeira letra maiúscula
+            
+            // DEBUG: Mostrar quantos produtos foram encontrados
+            // echo "<pre>Produtos encontrados: " . count($produtos) . "</pre>";
         } else {
             $produtos = Produto::getAll();
         }
@@ -18,6 +24,9 @@ class HomeController {
         // --- LINHA NOVA ---
         // Busca a lista de categorias para os filtros da sidebar
         $categorias_filtro = Produto::getAllCategorias(); 
+        
+        // DEBUG: Mostrar categorias disponíveis
+        // echo "<pre>Categorias disponíveis: " . var_export($categorias_filtro, true) . "</pre>";
 
         // Agora, a view `pagina_principal` terá acesso a $produtos, $titulo e $categorias_filtro
         require_once __DIR__ . '/../../views/pagina_principal.php';
